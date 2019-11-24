62f620f9b9814b82c5830071db3147a62e6bf101
Dariusz Wisniewski <dariusz.wisniewski@autodata-group.com>
2019-11-24 08:07:33 +0100

    add focus to loader

--- src/components/loader/loader.jsx
+++ src/components/loader/loader.jsx
@@ -2,17 +2,15 @@ import React, {useRef, useEffect, useState} from 'react';
 import './styles.scss';
 
 export default function Loader({hide}) {
-    // const []
-    // const inputRef = useRef(null);
-    // useEffect(() => {
-    //     if (isEditing) {
-    //         inputRef.current.focus();
-    //     }
-    // }, [isEditing]);
+    const inputRef = useRef(null);
+    useEffect(() => {
+        if (!hide) {
+            inputRef.current.focus();
+        }
+    }, [hide]);
     return (
-        <div role="status" className={`lds-spinner-container vh-100 w-100 position-fixed fixed-top bg-light ${hide ? 'd-none' : ''}`}>
-            <span>Ładowanie strony...</span>
-            <div className='lds-spinner d-flex m-auto'>
+        <div ref={inputRef} role="status" className={`lds-spinner-container vh-100 w-100 position-fixed fixed-top bg-light ${hide ? 'd-none' : ''}`}>
+            <div aria-label='Ładowanie strony...' className='lds-spinner d-flex m-auto'>
                 <div></div>
                 <div></div>
                 <div></div>
